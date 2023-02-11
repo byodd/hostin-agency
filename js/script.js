7c5cba9c9adb51c967c15fdcd0a6dfc22d9ab996
Gatta <gatta.code@gmail.com>
2023-02-11 16:45:12 +0100

    add animation on text

--- js/script.js
+++ js/script.js
@@ -1,18 +1,107 @@
 const header = document.querySelector("header");
-const div = document.querySelector("header div")
+const biotxt = document.querySelector(".bio");
+const biobig = document.querySelector(".biobig");
 
-window.addEventListener("scroll", () => {
+const illustration = document.querySelector(".p1z1 img");
+
+window.addEventListener("scroll", (e) => {
     if(window.scrollY >= 100){
         if(header.classList.contains("visible")){
             header.classList.remove("visible")
             header.style.height = "0rem"
-            div.style.opacity = 0;
         }
     }else{
         if(! header.classList.contains("visible")){
             header.classList.add("visible")
             header.style.height = "4.625rem"
-            div.style.opacity = 1;
         }
     }
+
+    if(window.scrollY >= 100){
+        biotxt.style.opacity = 1;
+    }
+
+    console.log(e.pageY)
+
+    if(window.scrollY >= 50){
+        illustration.style.transform = ("translateX(0%)")
+        illustration.style.opacity = 1;
+    }
+
+    if(window.scrollY >= 500){
+        biobig.style.opacity = 1;
+    }
 })
+
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  const scrollElements = document.querySelectorAll(".js-scroll");
+
+  const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+  
+    return (
+      elementTop <=
+      (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+  };
+  
+  const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+  
+    return (
+      elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
+  
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+  };
+  
+  const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+  };
+  
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 1.25)) {
+        displayScrollElement(el);
+      } else if (elementOutofView(el)) {
+        hideScrollElement(el)
+      }
+    })
+  }
+  
+  window.addEventListener("scroll", () => { 
+    handleScrollAnimation();
+  });
